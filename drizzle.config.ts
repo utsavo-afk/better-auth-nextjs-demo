6112cd6a97579ba03c46d4e66186128e664182c3
utsavojha <utsav.ojha@hotmail.com>
2025-06-16 12:15:49 +0530

    feat(drizzle.config.ts): update output directory for drizzle schema migrations to improve project structure and organization feat(drizzle.config.ts): add initial schema and migrations for accounts, sessions, users, verifications, and posts tables feat(drizzle.config.ts): add foreign key constraints between tables for data integrity and consistency

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import "./envConfig.ts";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./src/db/drizzle",
+  out: "./src/db/migrations",
   schema: "./src/db/schema/*",
   dialect: "postgresql",
   dbCredentials: {

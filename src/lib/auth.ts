e16935abc517526a8633d3391654db8a4a3290f4
utsavojha <utsav.ojha@hotmail.com>
2025-06-16 12:32:20 +0530

    feat(auth): refactor import statement in auth.ts to destructure db object from db module for better clarity and consistency

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@
 // all config lives here
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import db from "@/db";
+import { db } from "@/db";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
